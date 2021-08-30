62aa831428b5471cb4ab46b20644033009ed31d2
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2021-08-30 17:32:27 +0530

    Update index.php

--- simplelogin/index.php
+++ simplelogin/index.php
@@ -1,7 +1,7 @@
 <?php
 
-$password = "fuckyou";
-$username = "fuckyou";
+$password = "P455W0RD";
+$username = "U53RN4M3";
 $flag= "<br><br><h1 align='center' style='color: red'>hehe{y0u_g0t_th3_fl4g_h3h3}</h1>";
 $url = urldecode($_SERVER['REQUEST_URI']);
 $url = parse_url($url, PHP_URL_QUERY);
