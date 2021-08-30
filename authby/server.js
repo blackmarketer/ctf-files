fc1322a9a740de5eb4db1dcab9ccf80f3b935084
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2021-08-30 15:10:39 +0530

    Create server.js

---
+++ authby/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const app = express()
+
+app.use(cookieParser())
+
+const SESSION = {}
+
+const mustBeAuthenticated = (req, res, next) => {
+	if(req.cookies){
+	const token = req.cookies.token
+	console.log(SESSION[token])
+	if(token && SESSION[token]){
+		next()
+		}
+	}
+	res.send('not authorized')
+}
+
+app.get('/', mustBeAuthenticated, (req,res) => {
+	res.send('you made it')
+})
+
+app.listen(8080, () => console.log('running on 8080'))
