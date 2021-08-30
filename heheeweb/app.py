0dd3d0ce1a6285627acdb7509b2ceb0c44c2e7df
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2021-08-30 14:29:23 +0530

    Create app.py

---
+++ heheeweb/app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import web,sys,urllib,base64,StringIO
+
+url = ('/', 'index')
+site = web.template.render('index/')
+app = web.application(url, globals())
+
+class index:
+    def GET(self):
+        c = ''
+        guest = 'M52WK43U'
+        admin = 'MFSG22LO'
+        flag = "flag{fake}"
+        o = ''
+
+        stdout = sys.stdout
+        sys.stdout = reportSIO = StringIO.StringIO()
+
+        try:
+
+            get_input = web.input()
+            cookie = web.cookies().get('ci') if 'ci' in web.cookies() else None
+            if not cookie:
+                web.setcookie('ci', 'M52WK43U' , expires="", domain=None, secure=False)
+            if (cookie):
+                try:
+                    o = eval(base64.b32decode(str(cookie)))
+                    if (o == guest):
+                        c = str(c) + 'Not Authorised'
+                    elif (o == admin):
+                        c = str(c) + flag
+                    else:
+                        c = str(c) + str(o) + 'No user found'
+                except:
+                    pass
+	    if (c==''):
+		c='Unknown user'
+
+
+            reportStr = reportSIO.getvalue()
+            sys.stdout = stdout
+            output = str(reportStr) + str(c)
+            return site.index(output)
+
+        except Exception as error:
+
+            print(error)
+            return error
+
+
+
+
+if __name__ == "__main__":
+    app.run()
+
