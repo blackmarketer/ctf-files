7a8905c2eb36b2675f85e88a5bcc58e6b0922f72
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2021-09-11 19:49:18 +0530

    Update app.py

--- fassti/app.py
+++ fassti/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    g = request.args.get('g', 'anonymous')
+    g = request.args.get('name', 'anonymous')
     text = Template('<style>body {display: flex;align-items: center;justify-content: center;overflow: hidden;width: 100vw;height: 100vh;font-family: "Maitree", serif;}</style><h1 align="center">Welcome ' + g + '</h1>')
     return text.render()
   
