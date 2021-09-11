1ce7cdc266041fb18761405581eecf0ea5ffa7af
AkashSebastian <31592003+akashsebastian333@users.noreply.github.com>
2021-09-11 19:45:07 +0530

    Create app.py

---
+++ fassti/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+from jinja2 import Template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    g = request.args.get('g', 'anonymous')
+    text = Template('<style>body {display: flex;align-items: center;justify-content: center;overflow: hidden;width: 100vw;height: 100vh; color:000;background-color: var(--bg-color);font-family: "Maitree", serif;}</style><h1 align="center">Welcome ' + g + '</h1>')
+    return text.render()
+  
+if __name__ == "__main__":
+    app.run()
